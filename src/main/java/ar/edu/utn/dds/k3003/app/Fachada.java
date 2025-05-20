68eb7b9c5acebf051a7e69840a085cd9c5e4b2d7
ezequieljsosa <ezequieljsosa@gmail.com>
2025-05-20 02:16:10 -0300

    Tests de coleccion de fuentes funcionando

--- src/main/java/ar/edu/utn/dds/k3003/app/Fachada.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/Fachada.java
@@ -5,19 +5,39 @@ import ar.edu.utn.dds.k3003.facades.FachadaProcesadorPdI;
 import ar.edu.utn.dds.k3003.facades.dtos.ColeccionDTO;
 import ar.edu.utn.dds.k3003.facades.dtos.HechoDTO;
 import ar.edu.utn.dds.k3003.facades.dtos.PdIDTO;
+import ar.edu.utn.dds.k3003.model.Coleccion;
+import ar.edu.utn.dds.k3003.repository.ColeccionRepository;
+import ar.edu.utn.dds.k3003.repository.InMemoryColeccionRepo;
 import java.util.List;
 import java.util.NoSuchElementException;
+import lombok.val;
 
 public class Fachada implements FachadaFuente {
 
+  private ColeccionRepository coleccionRepository;
+
+  public Fachada() {
+    this.coleccionRepository = new InMemoryColeccionRepo();
+  }
+
   @Override
   public ColeccionDTO agregar(ColeccionDTO coleccionDTO) {
-    return null;
+    if (this.coleccionRepository.findById(coleccionDTO.nombre()).isPresent()){
+      throw  new IllegalArgumentException(coleccionDTO.nombre() + " ya existe");
+    }
+    val coleccion = new Coleccion(coleccionDTO.nombre(), coleccionDTO.descripcion());
+    this.coleccionRepository.save(coleccion);
+    return new ColeccionDTO(coleccion.getNombre(), coleccion.getDescripcion());
   }
 
   @Override
   public ColeccionDTO buscarColeccionXId(String coleccionId) throws NoSuchElementException {
-    return null;
+    val coleccionOptional = this.coleccionRepository.findById(coleccionId);
+    if(coleccionOptional.isEmpty()){
+      throw  new NoSuchElementException(coleccionId + " no existe");
+    }
+    val coleccion = coleccionOptional.get();
+    return new ColeccionDTO(coleccion.getNombre(),coleccion.getDescripcion());
   }
 
   @Override
